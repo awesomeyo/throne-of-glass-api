df640a372a7567f34ed734d0b952c843e50b046f
yeoyyl <yeoyyl92@gmail.com>
2022-01-02 19:59:06 +0800

    add api

---
+++ src/app.ts
@@ -0,0 +1,38 @@
+import express, { Request, Response } from "express";
+import mysql from "mysql";
+
+const app = express();
+
+const connectionString = process.env.DATABASE_URL || "";
+const connection = mysql.createConnection(connectionString);
+connection.connect();
+
+app.get("/api/characters", (req: Request, res: Response) => {
+  const query = "select * from Characters";
+  connection.query(query, (err, rows) => {
+    if (err) throw err;
+    const retVal = {
+      data: rows,
+      message: rows.lenght === 0 ? "No Record Found" : null,
+    };
+    return res.send(retVal);
+  });
+});
+app.get("/api/characters/:id", (req: Request, res: Response) => {
+  const id = req.params.id;
+  const query = "select * from Characters where ID = " + id + " LIMIT 1";
+  connection.query(query, (err, rows) => {
+    if (err) throw err;
+    const retVal = {
+      data: rows.length > 0 ? rows[0] : null,
+      message: rows.length === 0 ? "No Record Found" : null,
+    };
+    return res.send(retVal);
+  });
+});
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log("App is running");
+});
